fd9d7749d228f7d7ab97a65d0440a6ad17ec81c1
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-10-21 12:50:42 +0530

    Update dbtools.py

--- helpers/dbtools.py
+++ helpers/dbtools.py
@@ -25,7 +25,7 @@ async def handle_user_status(bot, cmd):
         await db.add_user(chat_id)
         await bot.send_message(
             LOG_CHANNEL,
-            f"**📣 bot notification.** \n\n#NEW_USER **start use your bot!** \n\n🏷 name: `{cmd.from_user.first_name}` \n📮 user id: `{cmd.from_user.id}` \n🧝🏻‍♂️ profile: [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})",
+            f"Bᴏᴛ Mᴇssᴀɢᴇ❗️ \n\n#NᴇᴡUsᴇʀAʀʀɪᴠᴇᴅ\n🔺Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ! \n\n📛 Nᴀᴍᴇ: `{cmd.from_user.first_name}` \n🆔 Tᴇʟᴇɢʀᴀᴍ Iᴅ: `{cmd.from_user.id}` \n🔗 Pʀᴏғɪʟᴇ Nᴀᴍᴇ: [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})",
         )
 
     ban_status = await db.get_ban_status(chat_id)
